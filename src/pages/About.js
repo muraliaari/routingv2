01e68958abb14d2865ae0e3104031ac0fa63b8bf
muraliaari <kunashaari@gmail.com>
2022-11-20 12:16:48 +0530

    Routing_loacal starage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 const About = () => {
   return (
     <div>
-        <Header/>
-        <div style={{backgroundColor:"pink",height:"200px",width:"800px",textAlign:"center"}}>
+        <Sidebar/>
+        <div style={{backgroundColor:"pink",height:"200px",width:"800px",textAlign:"center",marginLeft:"200px"} }>
           <h1>About Us</h1>
             <p>Get up to ₹6000 instant savings on orders over ₹41900. Plus No Cost EMI. Terms apply. iPhone, iPad, Mac, Apple Watch, AirPods and more. Buy Direct from Apple. Services: Apple Trade In, Shop with Specialists, Free delivery, No Cost EMI available.</p>
 
---
+++ src/pages/ToDo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Show from '../../../routing/src/components/Show'
+import Input from '../components/Input'
+const ToDo = () => {
+  return (
+    <div>
+        
+        <Input/>
+        
+    </div>
+  )
+}
+
+export default ToDo
